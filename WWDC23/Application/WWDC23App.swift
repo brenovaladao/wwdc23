311048e31be11ce08c4c573bd457e43044e086ec
Breno Valadão <brenovvaladao@gmail.com>
2023-06-21 09:46:34 +0200

    [feat]: Project setup

---
+++ WWDC23/Application/WWDC23App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC23App.swift
+//  WWDC23
+//
+//  Created by Breno Valadão on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WWDC23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WWDC23/Scenes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC23
+//
+//  Created by Breno Valadão on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
